f9a7d5c37737dc37af57a8b38e9a39c0dbc11ed2
Brandon Simpson <brandonsimpson@gmail.com>
2015-05-26 21:35:37 -0500

    add travis and codeclimate config

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,11 +7,19 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
+//echo $_SERVER['HTTP_CLIENT_IP'] . '<br />';
+//echo $_SERVER['REMOTE_ADDR'] . '<br />';
+//echo $_SERVER['HTTP_X_FORWARDED_FOR'] . '<br />';
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+
+$allowed_ips = array('127.0.0.1', 'fe80::1', '::1', '192.168.33.1');
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.33.1')) || php_sapi_name() === 'cli-server')
+    || ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !(in_array(@$_SERVER['HTTP_X_FORWARDED_FOR'], $allowed_ips)) )
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], $allowed_ips))
+    || php_sapi_name() === 'cli-server'
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
